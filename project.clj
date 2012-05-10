11cf23a18cee761bf4d85b020aa6a8fb0f80f3f0
Hugo Duncan <hugo@hugoduncan.org>
2012-05-10 10:27:06 -0400

    Change minimum clojure dependency to 1.2.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/pallet/pallet-map-merge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]]
   :profiles {:dev {:dependencies [[codox-md "0.1.0"]
                                   [codox/codox.core "0.6.2-SNAPSHOT"]]}}
   :codox {:writer codox-md.writer/write-docs
