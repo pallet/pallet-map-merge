165b25ebd1413a921320bd8a93bf8f722ce7634d
Hugo Duncan <hugo@hugoduncan.org>
2012-05-10 10:26:11 -0400

    Add codox to dev dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/pallet/pallet-map-merge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :profiles {:dev {:dependencies [[codox-md "0.1.0"]
+                                  [codox/codox.core "0.6.2-SNAPSHOT"]]}}
+  :codox {:writer codox-md.writer/write-docs
+          :output-dir "doc/0.1"})
