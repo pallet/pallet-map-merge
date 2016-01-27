c045f529fa1bb6c5407823a51f8594dd584791a0
Hugo Duncan <hugo@palletops.com>
2016-01-27 19:56:59 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pallet-map-merge "0.1.1"
+(defproject pallet-map-merge "0.1.2-SNAPSHOT"
   :description "Merging of clojure maps using per-key merge policies."
   :url "https://github.com/pallet/pallet-map-merge"
   :license {:name "Eclipse Public License"
